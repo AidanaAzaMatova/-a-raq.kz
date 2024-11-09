b74240e75b437106162f802f70386b2b90942e23
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-09 12:43:41 +0600

    first steps in playwright

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
-# main.py
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from models import User, Token
-
-# Конфигурация
-SECRET_KEY = "your_secret_key"  # Замените на ваш секретный ключ
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-app = FastAPI()
-
-# Хранилище пользователей (в реальном приложении используйте базу данных)
-users_db = {}
+# первые успехи в playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-# Создание токена
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    page.get_by_role("checkbox", name="Toggle Todo").check()
+
+    # ---------------------
+    context.close()
+    browser.close()
 
 
-@app.post("/auth/users/", status_code=200)
-async def create_user(user: User):
-    if user.username in users_db:
-        raise HTTPException(status_code=400, detail="User already exists")
-
-    users_db[user.username] = user
-    return {"message": "User created successfully"}
-
-
-@app.post("/auth/users/login", response_model=Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = users_db.get(form_data.username)
-    if not user or user.password != form_data.password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": form_data.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+with sync_playwright() as playwright:
+    run(playwright)
