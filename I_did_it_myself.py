b633c01c13e55a6dbce88932bb63dc51cb105ddc
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-10 19:52:28 +0600

    I did it myself

---
+++ I_did_it_myself.py
@@ -0,0 +1,30 @@
+# что то поменяла и вдруг заработал это какая то чудо
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    page.get_by_role("checkbox", name="Toggle Todo").check()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+def test_loc(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://zimaev.github.io/text_input/')
+    page.get_by_label("Email address").fill("qa@example.com")
+    page.get_by_title("username").fill("Anton")
+    page.get_by_placeholder('password').fill("secret")
+    page.get_by_role('checkbox').click()
+
+with sync_playwright() as page:
+    test_loc(page)
