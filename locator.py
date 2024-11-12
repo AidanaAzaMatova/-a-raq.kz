cc580a1a5b9c92d3af4d78079b6437571bc1379f
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-12 18:05:16 +0600

    locator

---
+++ locator.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+
+def test_locator_and() -> None:
+    playwright: Playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://zimaev.github.io/locatorand/")
+    selector = page.get_by_role("button", name="Sing up").and_(page.get_by_title("Sing up today"))
+    selector.click()
