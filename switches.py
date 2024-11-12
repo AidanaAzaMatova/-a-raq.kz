0d88ef1aa3fb982fa745c4fa0112622a5d07e235
Ai Doni <30952073+Aidoni0797@users.noreply.github.com>
2024-11-12 19:08:17 +0600

    switches

---
+++ switches.py
@@ -0,0 +1,14 @@
+# ну поставили эти чекбоксы что это даст можно и вручную сделать, хех
+import time
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://zimaev.github.io/checks-radios/')
+    page.locator("text=Default checkbox").check()
+    page.locator("text=Checked checkbox").check()
+    page.locator("text=Default radio").check()
+    page.locator("text=Default checked radio").check()
+    page.locator("text=Checked switch checkbox input").check()
+    time.sleep(10)
